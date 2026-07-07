777b4ecf21c549305ed25a1e3ee858dc1def28ba
randomusert <eetupajamaki@gmail.com>
2026-07-07 14:21:14 +0300

    more stuff

--- distro/init/init.c
+++ distro/init/init.c
@@ -1,9 +1,12 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main() {
     // Execute the init process
-    execl("/bin/cbash", NULL);
+    printf("Starting user-space....\n");
+    execl("/usr/bin/cbash", NULL);
 
     // If execl returns, there was an error
+    printf("Failed to start user-space or cbash closed. \n");
     return 1;
 }
